d739fc937ef99dc0fde872b33dc868374ed1bfe9
Abdul Bashir Rezaie <abdulbashirrezaie@gmail.com>
2022-12-04 14:11:56 +0430

    Create other schemas

--- schemas/schema.js
+++ schemas/schema.js
@@ -5,6 +5,10 @@ import createSchema from 'part:@sanity/base/schema-creator'
 import schemaTypes from 'all:part:@sanity/base/schema-type'
 
 import user from './user'
+import pin from './pin'
+import postedBy from './postedBy'
+import save from './save'
+import comment from './comment'
 
 // Then we give our schema to the builder and provide the result to Sanity
 export default createSchema({
@@ -13,6 +17,6 @@ export default createSchema({
   // Then proceed to concatenate our document type
   // to the ones provided by any plugins that are installed
   types: schemaTypes.concat([
-    user
+    user, pin, comment, postedBy, save
   ]),
 })
